3c26c1829011e4f7f819c8bafd70810f6b0032a0
wasimreza <wasim.cse.2k8@gmail.com>
2023-03-03 10:24:10 +0100

    flow extension example added

---
+++ app/src/main/java/com/example/wasim/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.wasim
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleCoroutineScope
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.example.wasim.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.FlowCollector
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        viewModel.getState().lifecycleAwareCollect(lifecycleScope, this) {
+            Timber.e(it)
+            binding.greetings.text = it
+        }
+    }
+}
+
+@HiltViewModel
+class MainViewModel @Inject constructor() : ViewModel() {
+    private val _state: MutableStateFlow<String> = MutableStateFlow("Hello")
+    private val state: StateFlow<String> = _state
+    fun getState() = state
+}
+
+fun <T> Flow<T>.lifecycleAwareCollect(
+    lifecycleScope: LifecycleCoroutineScope,
+    lifecycleOwner: LifecycleOwner,
+    collector: FlowCollector<T>,
+) {
+    val stateFlow = this
+    lifecycleScope.launch {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            stateFlow.collect(collector)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/wasim/TestActivity.kt
@@ -0,0 +1,97 @@
+package com.example.wasim
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Bundle
+import android.os.Looper
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
+import com.example.core.ext.viewBinding
+import com.example.wasim.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+import timber.log.Timber
+
+class TestActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by viewBinding(ActivityMainBinding::inflate)
+    private val locationRequest = LocationRequest.Builder(0L)
+        .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+        .setIntervalMillis(0L)
+        .setMinUpdateIntervalMillis(0L)
+        .setMaxUpdates(100)
+        .build()
+
+    @SuppressLint("MissingPermission")
+    fun FusedLocationProviderClient.locationFlow() = callbackFlow<Location> {
+        val callback = object : LocationCallback() {
+            override fun onLocationResult(result: LocationResult) {
+                try {
+                    result.lastLocation?.let { it1 -> trySend(it1).isSuccess }
+                } catch (e: Exception) {
+                }
+            }
+        }
+        if (checkPermission()) {
+            requestLocationUpdates(locationRequest, callback, Looper.getMainLooper())
+                .addOnFailureListener { e ->
+                    close(e) // in case of exception, close the Flow
+                }
+        } else {
+            requestLocationPermission()
+        }
+        // clean up when Flow collection ends
+        awaitClose {
+            removeLocationUpdates(callback)
+        }
+    }
+
+    private val requestMultiplePermissions =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) { permissions ->
+            if (permissions.entries.first().value && permissions.entries.last().value) {
+                val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+                fusedLocationClient.locationFlow().lifecycleAwareCollect(lifecycleScope, this) {
+                    Timber.e(it.toString())
+                    binding.greetings.text = it.toString()
+                }
+            }
+        }
+
+    private fun checkPermission(): Boolean {
+        return (
+            ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+               )
+    }
+
+    private fun requestLocationPermission() {
+        requestMultiplePermissions.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        requestLocationPermission()
+    }
+}
